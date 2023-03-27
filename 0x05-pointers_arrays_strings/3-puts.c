763f168d23eb975234aa5a42ab29a550c747e366
ihytimi <teemmymaj5@gmail.com>
2023-03-27 13:37:59 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - protects a string, followed by a newlie to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
