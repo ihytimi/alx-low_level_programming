5684036fab81d6b2ac95201ff0586e5e456896d5
ihytimi <teemmymaj5@gmail.com>
2023-04-09 01:32:44 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints its name followed y anew line
+ * @argc: integer value
+ * @argv: vector list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
