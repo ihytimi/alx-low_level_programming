ed9a0fb4fa7589514051ced988560113a2ea87b6
ihytimi <teemmymaj5@gmail.com>
2023-04-10 19:19:58 -0700

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
-		sum += atoi (argv[argc]);
+		sum += atoi(argv[argc]);
 
 	}
 	printf("%d\n", sum);
