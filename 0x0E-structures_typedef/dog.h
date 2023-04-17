89907094d5fa261c940ee26f1b92f037059504d1
ihytimi <teemmymaj5@gmail.com>
2023-04-17 11:42:51 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: dog's age
  * @owner: owner's name
  *
- * Description: just a lone dog struct in abig kitty world
+ * Description: just a lone dog struct in a big kitty world
  */
 
 struct dog
